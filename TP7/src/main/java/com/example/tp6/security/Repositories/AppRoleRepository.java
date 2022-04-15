e048ec64fa753a7904d4f75ae1c2bda604e9f648
salma khadidi <85806909+salma-didi@users.noreply.github.com>
2022-04-15 18:30:13 +0000

    Delete TP7 directory

--- TP7/src/main/java/com/example/tp6/security/Repositories/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tp6.security.Repositories;
-
-import com.example.tp6.security.Entities.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
-    AppRole findByRoleName(String roleName);
-}
